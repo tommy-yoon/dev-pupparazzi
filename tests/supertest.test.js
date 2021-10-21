f0593d39f8c5bba00bf36ee85637544b5f20713e
tommy-yoon <taeil.yoon@gmail.com>
2021-10-21 21:36:57 +1300

    add my branch

---
+++ tests/supertest.test.js
@@ -0,0 +1,43 @@
+const request = require('supertest')
+const cheerio = require('cheerio')
+
+const server = require('../server')
+
+// const router = require('./routes')
+
+test('test Get /', (done) => {
+  request(server)
+    .get('/')
+    .expect(200)
+    .end((err, res) => {
+      expect(err).toBeNull()
+      expect(res.text).toMatch('Pupparazzi')
+      done()
+    })
+})
+
+test('test Get /puppies/:id', (done) => {
+  request(server)
+    .get('/puppies/1')
+    .expect(200)
+    .end((err, res) => {
+      expect(err).toBeNull()
+      expect(res.text).toMatch('Pupparazzi')
+      done()
+    })
+  })
+
+  // testing using cheerio
+  // const $ = cheerio.load(res.text)
+  test('test GET / using cheerio', (done) => {
+    request(server)
+    .get('/')
+    .end((err, res) => {
+      expect(err).toBeNull()
+      const $ = cheerio.load(res.text)
+      // expect($('a').data('puppy').length).Not.toBeZero()
+      // const artistText = $('a').text()
+      // expect(artistText).toMatch('')
+      done()
+    })
+  })
